dd614513ba4e64f473623018243a5db9bd4c7bd2
Jonathan Martinez <alu0101254098@ull.edu.es>
2023-10-19 19:51:55 +0200

    Actualizando

--- p2.py
+++ p2.py
@@ -53,8 +53,3 @@ with open('output.txt', 'w') as output_file:
             idf = vectorizer.idf_[j]
             tfidf = tf * idf
             output_file.write(f'{j} | {term} | {tf:.4f} | {idf:.4f} | {tfidf:.4f}\n')
-        
-        output_file.write('Coseno Similarity with other documents:\n')
-        for j, similarity in enumerate(cosine_similarities[i]):
-            output_file.write(f'Document {j + 1}: {similarity:.4f}\n')
-        output_file.write('-' * 50 + '\n')
